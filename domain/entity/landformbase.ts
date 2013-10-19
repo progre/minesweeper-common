f441cdf0c3297d048e1eac0db860c81aee4af12e
progre <djyayutto@gmail.com>
2013-10-19 23:18:56 +0900

    update

--- domain/entity/landformbase.ts
+++ domain/entity/landformbase.ts
@@ -11,44 +11,44 @@ class LandformBase extends ee2.EventEmitter2 {
     // 地雷（地形）Map 普通の爆弾、☢、Bakuhigashi等
     // 解放状況(close, flag, open)
     /** @protected */
-    viewPointChunks = new ChunkCache<any>();
+    tileChunks = new ChunkCache<any>();
 
-    getViewPoint(coord: Coord) {
+    getTile(coord: Coord) {
         try {
-            return this.viewPointChunks.getShred(coord);
+            return this.tileChunks.getShred(coord);
         } catch (error) {
             if (error.name !== ChunkNotFoundError.name)
                 throw error;
             var e: ChunkNotFoundError = error;
-            this.requestViewPointChunk(e.coord);
+            this.requestTileChunk(e.coord);
             return null;
         }
     }
 
-    getViewPointChunk(coord: Coord) {
+    getTileChunk(coord: Coord) {
         try {
-            return this.viewPointChunks.getByCoord(coord);
+            return this.tileChunks.getByCoord(coord);
         } catch (error) {
             if (error.name !== ChunkNotFoundError.name)
                 throw error;
             var e: ChunkNotFoundError = error;
-            this.requestViewPointChunk(e.coord);
+            this.requestTileChunk(e.coord);
             return null;
         }
     }
 
-    getViewPointChunkFromGlobal(coord: Coord) {
-        return this.viewPointChunks.getByGlobal(coord);
+    getTileChunkFromGlobal(coord: Coord) {
+        return this.tileChunks.getByGlobal(coord);
     }
 
-    putViewPoint(coord: Coord, viewPoint: any) {
-        var updated = this.viewPointChunks.putShred(coord, viewPoint);
-        super.emit('view_point_updated', viewPoint);
+    putTile(coord: Coord, tile: any) {
+        var updated = this.tileChunks.putShred(coord, tile);
+        super.emit('view_point_updated', tile);
         return updated;
     }
 
-    putViewPointChunk(coord: Coord, chunk: Chunk<any>) {
-        var updated = this.viewPointChunks.putByCoord(coord, chunk);
+    putTileChunk(coord: Coord, chunk: Chunk<any>) {
+        var updated = this.tileChunks.putByCoord(coord, chunk);
         super.emit('chunk_updated', coord);
         return updated;
     }
@@ -58,6 +58,6 @@ class LandformBase extends ee2.EventEmitter2 {
 
     /** スーパークラスで実装必須 */
     /** @protected */
-    requestViewPointChunk(coord: Coord): void {
+    requestTileChunk(coord: Coord): void {
     }
 }
